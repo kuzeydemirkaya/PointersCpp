9256630742476bfff77c6dc0a27e671c99e7d643
kuzeydemirkaya <86108320+kuzeydemirkaya@users.noreply.github.com>
2021-06-18 14:59:27 +0300

    Add files via upload

---
+++ Pointers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    
+    int i=12;
+    int *pi;
+    
+    pi=&i;
+    *pi=56;
+        
+    cout<<*pi;
+    cout<<i;
+        
+    int x;
+    srand(time(NULL));
+    x=rand()%10+1;
+    cout<<x<<endl;
+    
+    cout<<rand()%10+1<<endl;
+
+    int dizi[]={3,4,5};
+    int *p1;
+    p1=&dizi[0];
+    cout<<"1. veri: "<<*p1<<endl;
+ 
+}
+
+
